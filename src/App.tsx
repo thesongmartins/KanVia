b92f042d8bbc199aaa2cf50b8a04e9a7bea4d8d1
thesongmartins <martinsjehoshaphat@gmail.com>
2025-06-02 20:24:29 +0100

    Task Management

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 const App = () => {
+  interface App {
+    id: number;
+    name: string;
+    task: string;
+  }
+
+  const task: App = {
+    id: 2,
+    name: "Song Martins",
+    task: "Todo",
+  };
   return <div>Task Management</div>;
 };
 
