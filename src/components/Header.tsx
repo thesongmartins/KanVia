bb45f49dd503f08dd14c5a28b04de711e3228f6e
thesongmartins <martinsjehoshaphat@gmail.com>
2025-06-22 01:16:57 +0100

    Fixing Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
       <header className="bg-gray-800 pt-6 text-white p-4 flex flex-row justify-between w-full border-l-2 border-gray-500">
         <h1>Platform Launch</h1>
         <button className="bg-purple-600 mr-6 py-2 px-5 rounded-2xl">
-          Add New Task
+          Add New Taskk
         </button>
       </header>
     </div>
